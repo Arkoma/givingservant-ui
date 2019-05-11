eb7ffb0b1fca46cfa5dd930c6f1be61983c0cc58
Aaron Burk <aarondburk@gmail.com>
2019-05-11 13:26:58 -0500

    initial commit

---
+++ src/main/java/org/goodservant/givingservant/GivingservantApplication.java
@@ -0,0 +1,13 @@
+package org.goodservant.givingservant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivingservantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GivingservantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/goodservant/givingservant/GivingservantApplicationTests.java
@@ -0,0 +1,16 @@
+package org.goodservant.givingservant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GivingservantApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
